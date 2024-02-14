78d997528fd40385cbc90f81523c1856f0785b84
Anuj Kaprawan <134187534+AnujKaprawan@users.noreply.github.com>
2024-02-14 20:02:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
-function calculateMinCost() {
-
-  var inputElement = document.getElementById('rope-lengths').value;
-  var arr = inputElement.split(',');
-   arr.sort(function (a,b) {return a-b});
-
- var cost = 0;
- while (arr.length>1){
-  var res = Number(arr[0])+ Number(arr[1]);
-        arr.shift()
-        arr.shift()
-  arr.push(res);
-  cost +=res;
-
+function mincost(arr)
+{ 
+function mincost(arr) {
+    // Convert the input array to a min-heap
+    const heapify = (array) => {
+      for (let i = Math.floor(array.length / 2); i >= 0; i--) {
+        heapifyDown(array, i);
+      }
+    };
   
-
-  arr.sort(function (a,b) {return a-b})
- }
-  //your code here
-  document.getElementById("result").innerHTML = cost
+    const heapifyDown = (array, i) => {
+      const left = 2 * i + 1;
+      const right = 2 * i + 2;
+      let smallest = i;
+  
+      if (left < array.length && array[left] < array[smallest]) {
+        smallest = left;
+      }
+  
+      if (right < array.length && array[right] < array[smallest]) {
+        smallest = right;
+      }
+  
+      if (smallest !== i) {
+        [array[i], array[smallest]] = [array[smallest], array[i]];
+        heapifyDown(array, smallest);
+      }
+    };
+  
+    heapify(arr);
+  
+   
+    let totalCost = 0;  
+   
+    while (arr.length > 1) {
+     
+      const rope1 = arr.shift();
+      const rope2 = arr.shift();
+       
+      const cost = rope1 + rope2;
+       
+      totalCost += cost;
+       
+      arr.push(cost);
+      heapifyDown(arr, 0);
+    }
+  
+    return totalCost;
+  }
+  
   
 }
+
+module.exports=mincost;
